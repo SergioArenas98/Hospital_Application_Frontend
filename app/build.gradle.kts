8001faafaf7ff404cb54598650b61074f3550501
SergioArenas98 <sejuma18@gmail.com>
2024-11-22 18:37:44 +0100

    find nurses by name implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.sejuma.hospitalapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.sejuma.hospitalapplication"
