7ea7c32d61984866126c252701fdf56b6c3a131d
DavidSiles <davidsiles216@gmail.com>
2024-12-11 12:02:22 +0100

    register screen

---
+++ app/src/main/java/com/sejuma/hospitalapplication/NurseRegisterScreen.kt
@@ -0,0 +1,131 @@
+package com.sejuma.hospitalapplication
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.sejuma.hospitalapplication.model.Nurse
+import com.sejuma.hospitalapplication.viewmodel.NurseViewModel
+
+@Composable
+fun NurseRegisterScreen(navController: NavHostController, nurseViewModel: NurseViewModel = viewModel()){
+
+    var name by remember { mutableStateOf("") }
+    var user by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var imageRes by remember { mutableStateOf(0) }
+    var registerSuccess by remember { mutableStateOf(false) }
+    var showMessage by remember { mutableStateOf(false) }
+
+    val nurses by nurseViewModel.nurses.observeAsState(emptyList())
+
+    fun validateCredentials(user: String): Boolean {
+        return !nurses.any{it.user == user}
+    }
+
+    Button(onClick = {
+        navController.navigate("NurseLoginScreen")
+    }) {
+        Text(text = "Back to Login")
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Welcome to Register",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+    // name
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text(text = "Nurse Name") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+    // user
+        OutlinedTextField(
+            value = user,
+            onValueChange = { user = it },
+            label = { Text(text = "User") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+    // password
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+    // imagen
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            registerSuccess = validateCredentials(user)
+            showMessage = true
+            if (registerSuccess) {
+                var  newNurse: Nurse = Nurse(name, user, password, imageRes)
+                nurseViewModel.addNurse(newNurse)
+                navController.navigate("homeScreen")
+            }}) {
+            Text(text = stringResource(id = R.string.registerButton))
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        if (showMessage) {
+            if (registerSuccess) {
+                Text(
+                    text = "You logged in successfully!",
+                    fontSize = 18.sp,
+                    color = Color.Green
+                )
+            } else {
+                Text(
+                    text = "Invalid credentials",
+                    fontSize = 18.sp,
+                    color = Color.Red
+                )
+            }
+        }
+    }
+}
